d70f3bf66450f8084daf91335429c6a4d9864154
Wayne Mao <helloint@qq.com>
2024-06-04 00:00:47 +0800

    feat: add file cache

--- scripts/checkHeroSize.js
+++ scripts/checkHeroSize.js
@@ -1,19 +1,45 @@
 import fs from 'fs';
+import {getCurrentDateTimeStringPath, readDataFile, writeDataFile} from "./utils.js";
 
 const main = async () => {
 	const { GITHUB_OUTPUT } = process.env;
-	const url = 'https://ottapp-appgw-client-a.proda.msn.tv3cloud.com/S1/discovery/v3/feeds/1ad86f5a-c5d4-41de-89c0-2819b56fa01e/items?$groups=4000000,4000001,1027&storeId=HubsAndFeeds-Main';
+	const filenameV3 = 'v3/feeds/1ad86f5a-c5d4-41de-89c0-2819b56fa01e/items';
+	const filenameV4 = 'v4/feeds/1ad86f5a-c5d4-41de-89c0-2819b56fa01e/items';
+	const resultV3 = await monitorFile(filenameV3);
+	const resultV4 = await monitorFile(filenameV4);
+	if (resultV3 !== null || resultV4 !== null) {
+		fs.appendFileSync(GITHUB_OUTPUT, `new_file=1`);
+	}
+	if (resultV3 > 1) {
+		fs.appendFileSync(GITHUB_OUTPUT, `data=${resultV3.length}`);
+	} else if (resultV4 > 1) {
+		fs.appendFileSync(GITHUB_OUTPUT, `data=${resultV4.length}`);
+	}
+}
+
+const monitorFile = async (filename) => {
+	const server = 'https://ottapp-appgw-client-a.proda.msn.tv3cloud.com/S1/discovery/';
+	const url = `${server}${filename}?$groups=4000000,4000001,1027&storeId=HubsAndFeeds-Main`;
 	const response = await fetch(url);
 	if (!response.ok) {
 		throw new Error('Network response was not ok');
 	}
 
 	try {
-		const jsonData = await response.json();
-		fs.appendFileSync(GITHUB_OUTPUT, `data=${jsonData.length}`);
- 	} catch (error) {
+		const newData = await response.json();
+		const currentFile = `${filename.replace('/', '_')}/current.json`;
+		const previousData = readDataFile(currentFile, 'utf8');
+		if (JSON.stringify(previousData) !== JSON.stringify(newData)) {
+			const timestamp = getCurrentDateTimeStringPath();
+			writeDataFile(currentFile, newData);
+			writeDataFile(`${filename.replace('/', '_')}/${timestamp}`, newData);
+			return newData.length;
+		}
+	} catch (error) {
 		console.error('There was a problem with your fetch operation:', error);
 	}
+
+	return null;
 }
 
 main();
---
+++ scripts/utils.js
@@ -0,0 +1,55 @@
+// ./scripts/utils.js
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Needed to get __filename and __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const writeDataFile = (relativeFilePath, data) => {
+	try {
+		// Resolve the file path relative to the ./data/ directory
+		const dataDir = path.resolve(__dirname, '../data'); // Adjust as necessary to get the data directory
+		const filePath = path.join(dataDir, relativeFilePath);
+
+		// Ensure the directory exists
+		const dir = path.dirname(filePath);
+		fs.mkdirSync(dir, { recursive: true });
+
+		// Write the file
+		fs.writeFileSync(filePath, data, 'utf8');
+
+		console.log('File written successfully');
+	} catch (error) {
+		console.error('Error writing file', error);
+	}
+};
+
+export const readDataFile = (relativeFilePath) => {
+	// Resolve the file path relative to the ./data/ directory
+	const dataDir = path.resolve(__dirname, '../data'); // Adjust as necessary to get the data directory
+	const filePath = path.join(dataDir, relativeFilePath);
+
+	try {
+		const data = fs.readFileSync(filePath, 'utf8');
+		return JSON.parse(data);
+	} catch (error) {
+		// If the file doesn't exist or is empty, return an empty object
+		return {};
+	}
+};
+
+export const getCurrentDateTimeStringPath = () => {
+	const now = new Date();
+
+	const year = now.getFullYear().toString().slice(-2); // Get last 2 digits of the year
+	const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-based, so add 1
+	const day = String(now.getDate()).padStart(2, '0');
+	const hours = String(now.getHours()).padStart(2, '0');
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+
+	return `${year}/${month}/${day}/${hours}-${minutes}.json`;
+}
+
