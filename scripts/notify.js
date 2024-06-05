8463cfc87c89d7f5cbf651955935fe68034b5e3b
Wayne Mao <helloint@qq.com>
2024-06-05 17:57:57 +0800

    optimize

--- scripts/notify.js
+++ scripts/notify.js
@@ -1,23 +1,26 @@
 async function main() {
-	if (process.env.msg && process.env.notify_server && process.env.notify_token) {
-		const text = JSON.parse(process.env.msg);
+	const msg = process.env.msg;
+	const notifyServer = process.env.notify_server;
+	const notifyToken = process.env.notify_token;
+	if (msg && notifyServer && notifyToken) {
+		const text = JSON.parse(msg);
 
 		const data = {
 			payload: JSON.stringify({
 				text
 			}),
-			token: process.env.notify_token
+			token: notifyToken
 		};
 
 		const options = {
 			method: 'POST',
 			body: Object.entries(data).map(([key, value]) => `${key}=${value}`).join('&')
 		};
-		await fetch(`${process.env.notify_server}/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2`, options);
+		await fetch(`${notifyServer}/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2`, options);
 	} else {
-		if (!process.env.notify_server) console.log('notify_server required');
-		if (!process.env.notify_token) console.log('notify_token required');
-		if (!process.env.msg) console.log('msg required');
+		if (!notifyServer) console.log('notify_server required');
+		if (!notifyToken) console.log('notify_token required');
+		if (!msg) console.log('msg required');
 	}
 }
 
